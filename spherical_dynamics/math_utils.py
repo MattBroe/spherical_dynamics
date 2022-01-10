cf8600d7b49391f5f8c16105e6581503780ad550
Matt <dev.lasquemas@gmail.com>
2022-01-09 19:23:30 -0800

    Bump function

---
+++ spherical_dynamics/math_utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def bump(x):
+    if np.absolute(x) >= 1:
+        return 0
+
+    return np.exp(-1 / (1 - x * x))
+
+
--- spherical_dynamics/spherical_dynamics.py
+++ spherical_dynamics/spherical_dynamics.py
@@ -1,1 +1,15 @@
+import math_utils as mu
+import numpy as np
 
+def main():
+    breakpoint()
+    x = np.float32(0)
+
+    while True:
+        output = mu.bump(x)
+        print("Input: {0} Output: {1}", x, output)
+        x = .5 + (x / 2)
+        breakpoint()
+
+if __name__ == "__main__":
+    main()
